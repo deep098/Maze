a4d35f3294e0407384d23293550c71cde11b6e66
deep098 <deepakvagish@gmail.com>
2022-10-12 00:10:56 -0400

    Added modified DFS functionality

--- maze.py
+++ maze.py
@@ -1,18 +1,40 @@
-from re import X
 import numpy as np
 class Maze:
     def __init__(self, dimension, probability):
         self.dimension = dimension
         self.probability = probability
-        self.grid = np.array([])
-    
+
     def generate_grid(self):
-        random_arr = np.random.rand(self.dimension, self.dimension)
-        random_arr = random_arr > self.probability
-        random_arr[0][0], random_arr[-1][-1] = True, True
-        grid = [[Cell(i, j, random_arr[i][j]) for j in range(self.dimension)] for i in range(self.dimension)]
+        blocked_grid = self.generate_dfs()
+        blocked_grid[0][0], blocked_grid[-1][-1] = True, True
+        grid = [[Cell(i, j, blocked_grid[i][j]) for j in range(self.dimension)] for i in range(self.dimension)]
         return grid 
     
+    
+    def generate_dfs(self):
+        grid = np.random.rand(self.dimension, self.dimension)
+        visited = np.full(grid.shape, False)
+        final_grid = np.full(grid.shape, False)
+        stack = []
+        stack.append({'x':0, 'y':0})
+        while stack:
+            last = stack.pop()
+            x, y = last['x'], last['y']
+            if visited[last['x']][last['y']]:
+                continue
+            visited[x][y] = True
+            if np.random.rand() <= self.probability:
+                final_grid[x][y] = True
+            if y - 1 >= 0:
+                stack.append({'x': x, 'y': y - 1})
+            if y + 1 < len(grid):
+                stack.append({'x': x, 'y': y + 1})
+            if x - 1 >= 0:
+                stack.append({'x': x - 1, 'y': y})
+            if x + 1 < len(grid):
+                stack.append({'x': x + 1, 'y': y})
+        
+        return final_grid
 
 class Cell:
     def ___init__(self, x, y, block):
